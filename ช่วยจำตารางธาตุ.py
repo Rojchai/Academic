b074dd32a7043f421f56d8b6f2c25f55fd5d1d51
Rojchai <110082568+Rojchai@users.noreply.github.com>
2023-12-06 19:01:12 +0700

    Create ช่วยจำตารางธาตุ.py

---
+++ ช่วยจำตารางธาตุ.py
@@ -0,0 +1,264 @@
+import random
+#1
+H = "ไฮโดรเจน1"
+Li = "ลิเทียม1"
+Na = "โซเดียม1"
+K = "โพแทสเซียม1"
+Rb = "รูบิเดียม1"
+Cs = "ซีเซียม1"
+Fr = "แฟรนเซียม1"
+#2
+Be = "เบริลเลียม2"
+Mg = "แมกนีเซียม2"
+Ca = "แคลเซียม2"
+Sr = "สทรอนเทียม2"
+Ba = "แบเรียม2"
+Ra = "เรเดียม2"
+#3
+B = "โบรอน3"
+Al = "อะลูมิเนียม3"
+Ga = "แกลเลียม3"
+In = "อินเดียม3"
+Tl = "แทลเลียม3"
+Nh = "นิโฮเนียม3"
+#4
+C = "คาร์บอน4"
+Si = "ซิลิคอน4"
+Ge = "เจอร์เมเนียม4"
+Sn = "ทิน(ดีบุก)4"
+Pb = "เลด(ตะกั่ว)4"
+Fl = "ฟลิโรเวียม4"
+#5
+N = "ไนโตรเจน5"
+P = "ฟอสฟอรัส5"
+As = "อาร์เซนิก(สารหนู)5"
+Sb = "แอนติโมนี(พลวง)5"
+Bi = "บิสมัท6"
+Mc = "มอสโกเวียม5"
+#6
+O = "ออกซิเจน6"
+S = "ซัลเฟอร์(กำมะถัน)6"
+Se = "ซีลีเนียม6"
+Te = "เทบลูเรียม6"
+Po = "พอโลเวียม6"
+Lv = "ลิเวอร์มอเรียม6"
+#7
+F = "ฟลูออรีน7"
+Cl = "คลอรีน7"
+Br = "โบรมีน7"
+I = "ไอโอดีน7"
+At = "แอสทาทีน7"
+Ts = "เทนเนสซีน7"
+#8
+He = "ฮีเลียม8"
+Ne = "นีออน8"
+Ar = "อาร์กอน8"
+Kr = "คริปทอน8"
+Xe = "ซีนอน8"
+Rn = "เรดอน8"
+Og = "โอกาเนสซอน"
+while True:
+    print("------------------------------")
+    rdr = random.randint(1, 50)
+    if rdr == 1 :
+        print("            H")
+        input("            ")
+        print("            ",H)
+    elif rdr == 2 :
+        print("            Li")
+        input("            ")
+        print("            ",Li)
+    elif rdr == 3 :
+        print("            Na")
+        input("            ")
+        print("            ",Na)
+    elif rdr == 4 :
+        print("            K")
+        input("            ")
+        print("            ",K)
+    elif rdr == 5 :
+        print("            Rb")
+        input("            ")
+        print("            ",Rb)
+    elif rdr == 6 :
+        print("            Cs")
+        input("            ")
+        print("            ",Cs)
+    elif rdr == 7 :
+        print("            Fr")
+        input("            ")
+        print("            ",Fr)
+    elif rdr == 8 :
+        print("            Be")
+        input("            ")
+        print("            ",Be)
+    elif rdr == 9 :
+        print("            Mg")
+        input("            ")
+        print("            ",Mg)
+    elif rdr == 10 :
+        print("            Ca")
+        input("            ")
+        print("            ",Ca)
+    elif rdr == 11 :
+        print("            Sr")
+        input("            ")
+        print("            ",Sr)
+    elif rdr == 12 :
+        print("            Ba")
+        input("            ")
+        print("            ",Ba)
+    elif rdr == 13 :
+        print("            Ra")
+        input("            ")
+        print("            ",Ra)
+    elif rdr == 14 :
+        print("            B")
+        input("            ")
+        print("            ",B)
+    elif rdr == 15 :
+        print("            Al")
+        input("            ")
+        print("            ",Al)
+    elif rdr == 16 :
+        print("            Ga")
+        input("            ")
+        print("            ",Ga)
+    elif rdr == 17 :
+        print("            In")
+        input("            ")
+        print("            ",In)
+    elif rdr == 18 :
+        print("            Tl")
+        input("            ")
+        print("            ",Tl)
+    elif rdr == 19 :
+        print("            Nh")
+        input("            ")
+        print("            ",Nh)
+    elif rdr == 20 :
+        print("            C")
+        input("            ")
+        print("            ",C)
+    elif rdr == 21 :
+        print("            Si")
+        input("            ")
+        print("            ",Si)
+    elif rdr == 22 :
+        print("            Ge")
+        input("            ")
+        print("            ",Ge)
+    elif rdr == 23 :
+        print("            Sn")
+        input("            ")
+        print("            ",Sn)
+    elif rdr == 24 :
+        print("            Pb")
+        input("            ")
+        print("            ",Pb)
+    elif rdr == 25 :
+        print("            Fl")
+        input("            ")
+        print("            ",Fl)
+    elif rdr == 26 :
+        print("            N")
+        input("            ")
+        print("            ",N)
+    elif rdr == 27 :
+        print("            P")
+        input("            ")
+        print("            ",P)
+    elif rdr == 28 :
+        print("            As")
+        input("            ")
+        print("            ",As)
+    elif rdr == 29 :
+        print("            Sb")
+        input("            ")
+        print("            ",Sb)
+    elif rdr == 30 :
+        print("            Bi")
+        input("            ")
+        print("            ",Bi)
+    elif rdr == 31 :
+        print("            Mc")
+        input("            ")
+        print("            ",Mc)
+    elif rdr == 32 :
+        print("            O")
+        input("            ")
+        print("            ",O)
+    elif rdr == 33 :
+        print("            S")
+        input("            ")
+        print("            ",S)
+    elif rdr == 34 :
+        print("            Se")
+        input("            ")
+        print("            ",Se)
+    elif rdr == 35 :
+        print("            Te")
+        input("            ")
+        print("            ",Te)
+    elif rdr == 36 :
+        print("            Po")
+        input("            ")
+        print("            ",Po)
+    elif rdr == 37 :
+        print("            Lv")
+        input("            ")
+        print("            ",Lv)
+    elif rdr == 38 :
+        print("            F")
+        input("            ")
+        print("            ",F)
+    elif rdr == 39 :
+        print("            Cl")
+        input("            ")
+        print("            ",Cl)
+    elif rdr == 40 :
+        print("            Br")
+        input("            ")
+        print("            ",Br)
+    elif rdr == 41 :
+        print("            I")
+        input("            ")
+        print("            ",I)
+    elif rdr == 42 :
+        print("            At")
+        input("            ")
+        print("            ",At)
+    elif rdr == 43 :
+        print("            Ts")
+        input("            ")
+        print("            ",Ts)
+    elif rdr == 44 :
+        print("            He")
+        input("            ")
+        print("            ",He)
+    elif rdr == 45 :
+        print("            Ne")
+        input("            ")
+        print("            ",Ne)
+    elif rdr == 46 :
+        print("            Ar")
+        input("            ")
+        print("            ",Ar)
+    elif rdr == 47 :
+        print("            Kr")
+        input("            ")
+        print("            ",Kr)
+    elif rdr == 48 :
+        print("            Xe")
+        input("            ")
+        print("            ",Xe)
+    elif rdr == 49 :
+        print("            Rn")
+        input("            ")
+        print("            ",Rn)
+    elif rdr == 50 :
+        print("            Og")
+        input("            ")
+        print("            ",Og)
+    else :
+        print("...")
